4a24b900a4fc5e7416ca045aaa200795adc59e11
shukrani-R <ShukraniRaymondi@gmail.com>
2025-07-11 10:59:04 +0300

    Update life.js

--- plugins/life.js
+++ plugins/life.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const { ezra } = require("../fredi/ezra");
-ezra({ nomCom: "altest", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+const { ezra } = require("../shukrani/ray");
+ray({ nomCom: "altest", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '🅼🆈 🅽🅰🅼🅴 🅸🆂 *🅻🆄🅲🅺🆈 🅼🅳* ғᴇʀʀᴀʀɪ 🚗 \n\n ' + "i'm a whatsapp bot multi-device created ";
+    let z = '🅼🆈 🅽🅰🅼🅴 🅸🆂 *🆁🅰🆈 🅼🅳* ғᴇʀʀᴀʀɪ 🚗 \n\n ' + "i'm a whatsapp bot multi-device created ";
     let d = ' by *ғʀᴇᴅɪᴇᴢʀᴀ255*';
     let varmess = z + d;
-    var img = 'https://files.catbox.moe/7irwqn.jpeg';
+    var img = 'https://files.catbox.moe/b2uxhi.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
@@ -19,9 +19,9 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Hi my name is *LUCKY MD* \n\n '+' I'm a Multi-device Whatsapp bot '
+   let z ='Hi my name is *RAY MD* \n\n '+' I'm a Multi-device Whatsapp bot '
       let d =' developed by *frediezra255*'
       let varmess=z+d
-      var img='https://files.catbox.moe/7irwqn.jpeg'
+      var img='https://files.catbox.moe/b2uxhi.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
 }  */ 
