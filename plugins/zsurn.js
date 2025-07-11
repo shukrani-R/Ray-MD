a64eb4498949b66087169b2eb454ec5cac870edc
shukrani-R <ShukraniRaymondi@gmail.com>
2025-07-12 00:13:04 +0300

    Update zsurn.js

--- plugins/zsurn.js
+++ plugins/zsurn.js
@@ -1,8 +1,8 @@
-const { ezra } = require('../fredi/ezra');
+const { ray } = require('../shukrani/ray');
 const axios = require('axios');
 const conf = require(__dirname + "/../set");
 
-ezra({
+ray({
   nomCom: "surah",
  aliases: ["surahh", "qurann"],
   reaction: '🤲',
@@ -46,7 +46,7 @@ ezra({
     
     const data = response.data.data;
     const messageText = `
-ᬑ *LUCKY QURAN SURAH* ᬒ
+ᬑ *RAY QURAN SURAH* ᬒ
 
 *🕌 Quran: The Holy Book*
 📜 *Surah:* 🕌❤️${data.number}: ${data.asma.ar.long} (${data.asma.en.long})❤️🕌
@@ -62,7 +62,7 @@ ezra({
       text: messageText,
       contextInfo: {
         externalAdReply: {
-          title: "LUCKY QURAN SURAH",
+          title: "RAY QURAN SURAH",
           body: `We're reading: ${data.asma.en.long}`,
           mediaType: 1,
           thumbnailUrl: conf.URL, 
@@ -79,7 +79,7 @@ ezra({
         externalAdReply: {
           title: "Error Fetching Quran Passage",
           body: "Please try again later.",
-          thumbnailUrl: "https://files.catbox.moe/058ggv.jpg", // Replace with a suitable thumbnail URL
+          thumbnailUrl: "https://files.catbox.moe/b2uxhi.jpg", // Replace with a suitable thumbnail URL
           sourceUrl: conf.GURL,
           mediaType: 1,
           showAdAttribution: true,
