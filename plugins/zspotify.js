2e0cb017112d836f6af70ccf63632b67f335de2e
shukrani-R <ShukraniRaymondi@gmail.com>
2025-07-12 00:21:28 +0300

    Update zspotify.js

--- plugins/zspotify.js
+++ plugins/zspotify.js
@@ -1,7 +1,7 @@
-const { ezra } = require('../fredi/ezra');
+const { ray } = require('../shukrani/ray');
 const axios = require("axios");
 
-ezra({
+ray({
   nomCom: "spotifylist",
   aliases: ["spotifysearch", "splaylist"],
   categorie: "Fledi-search",
@@ -25,7 +25,7 @@ ezra({
     }
 
     // Construct playlist message
-    let playlistMessage = `LUCKY PLANET SPOTIFY PLAY\n\n`;
+    let playlistMessage = `RAY PLANET SPOTIFY PLAY\n\n`;
 
     // Loop through search results and construct track info with numbers
     searchData.forEach((track, index) => {
@@ -46,9 +46,9 @@ ezra({
           mentionedJid: [dest],  // Mention the sender's JID
           externalAdReply: {
             showAdAttribution: true,
-            title: "LUCKY MD SPOTIFY PLAY",
-            body: "Credit by frediezra",
-            sourceUrl: "https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f",
+            title: "RAY MD SPOTIFY PLAY",
+            body: "Credit by shukraniray",
+            sourceUrl: "https://whatsapp.com/channel/0029VbB16dt9hXEyw3bO1k0p",
             mediaType: 1,
             renderLargerThumbnail: false,
           },
