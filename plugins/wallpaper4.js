c7ba8c95b41d216d4dad1413c851eba5666cc659
shukrani-R <ShukraniRaymondi@gmail.com>
2025-07-11 21:40:00 +0300

    Update wallpaper.js

--- plugins/wallpaper4.js
+++ plugins/wallpaper4.js
@@ -4,10 +4,10 @@ Object.defineProperty(exports, '__esModule', {
   'value': true
 });
 const {
-  ezra
-} = require("../fredi/ezra");
-ezra({
-  'nomCom': "wallpaper4",
+  ray
+} = require("../shukrani/ray");
+ray({
+  'nomCom': "wallpaper",
   'reaction': '🎞',
   'nomFichier': __filename
 }, async (_0x4388c3, _0x2d564f, _0x51d5cb) => {
@@ -16,7 +16,7 @@ ezra({
     'image': {
       'url': 'https://telegra.ph/file/6710065448c2367f23fcb.jpg'
     },
-    'caption': "🚗𝗥𝗮𝗻𝗱𝗼𝗺 𝘄𝗮𝗹𝗹𝗽𝗮𝗽𝗲𝗿 \n\n 🚘Download it and set it to your wallpaper 𝗙𝗿𝗼𝗺 (LUCKY-𝗠𝗗)🚗MADE BY FREDIE ADMIN"
+    'caption': "🚗𝗥𝗮𝗻𝗱𝗼𝗺 𝘄𝗮𝗹𝗹𝗽𝗮𝗽𝗲𝗿 \n\n 🚘Download it and set it to your wallpaper 𝗙𝗿𝗼𝗺 (RAY-𝗠𝗗)🚗MADE BY SHUKRANI ADMIN"
   });
 });
 console.log("mon test");
